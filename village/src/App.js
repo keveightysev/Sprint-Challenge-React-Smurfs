51f703e5a3880332f5a6de791b22ffcfee812cfc
Kevin Smith <keveightysev@gmail.com>
2019-03-01 10:25:09 -0600

    Built an axios request in CDM tor retrieve smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -10,6 +11,15 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await axios.get('http://localhost:3333/smurfs');
+      this.setState(res.data);
+    } catch (err) {
+      console.log(err.message);
+    }
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
