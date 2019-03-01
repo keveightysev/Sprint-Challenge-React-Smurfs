d752e4b72ed6ebc561beb4cdaf080ac243af41e2
Kevin Smith <keveightysev@gmail.com>
2019-03-01 12:43:59 -0600

    Added update functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -3,6 +3,13 @@ import axios from 'axios';
 
 class Smurf extends React.Component {
 
+  state = {
+    name: '',
+    age: '',
+    height: '',
+    updateClicked: false,
+  }
+
   removeSmurf = async (e, id) => {
     e.preventDefault();
     const res = await axios.delete(`http://localhost:3333/smurfs/${id}`);
@@ -10,18 +17,46 @@ class Smurf extends React.Component {
     this.props.history.push('/');
   }
 
+  updateSmurf = async (e, id) => {
+    e.preventDefault();
+    const smurfInfo = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height,
+    }
+    const res = await axios.put(`http://localhost:3333/smurfs/${id}`, smurfInfo)
+    this.props.updateParent(res.data);
+  }
+
+  handleUpdateBtn = e => {
+    e.preventDefault();
+    this.setState({ updateClicked: !this.state.updateClicked })
+  }
+
+  handleChange = e => {
+    const value = e.target.name === "age" ? Number(e.target.value) : e.target.value;
+    this.setState({ [e.target.name]: value })
+  }
+
   render() {
     const smurf = this.props.smurfs.find(smurf => `${smurf.id}` === this.props.match.params.id);
     if (!smurf) {
       return <h3>Loading Smurf Info...</h3>;
     }
-    
+
     return (
       <div className="Smurf">
         <h3>{smurf.name}</h3>
         <strong>{smurf.height} tall</strong>
         <p>{smurf.age} smurf years old</p>
         <button onClick={e => this.removeSmurf(e, smurf.id)}>Remove Smurf</button>
+        <button onClick={this.handleUpdateBtn}>Update Smurf</button>
+        {this.state.updateClicked ? <form onSubmit={e => this.updateSmurf(e, smurf.id)}>
+          <input type="text" value={this.state.name} name="name" onChange={this.handleChange}/>
+          <input type="number" value={this.state.age} name="age" onChange={this.handleChange}/>
+          <input type="text" value={this.state.height} name="height" onChange={this.handleChange}/>
+          <button>Send Changes</button>
+        </form> : null}
       </div>
     );
   }
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -11,7 +11,7 @@ class Smurfs extends Component {
         <ul>
           {this.props.smurfs.map(smurf => {
             return (
-              <Link to={`/smurf/${smurf.id}`}>{smurf.name}</Link>
+              <Link to={`/smurf/${smurf.id}`} key={smurf.id}>{smurf.name}</Link>
               // <Smurf
               //   {...smurf}
               //   key={smurf.id}
